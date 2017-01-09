b154f8b490bae66270fd1d60c7206fdf3c1f3c98
Hasan <hasan_wajahat@hotmail.com>
2017-01-09 19:59:56 +0500

    added navigation bar basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { BannerComponent } from './header/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
