250d1553266ccd4707fb38cb85749c62661a6738
Hasan <hasan_wajahat@hotmail.com>
2017-01-12 02:58:56 +0500

    added images to featured sites

--- src/app/main/landing/featured/featuredData.ts
+++ src/app/main/landing/featured/featuredData.ts
@@ -1,19 +1,23 @@
 export var featuredData =
     [{
         "place": "Kashimir",
-        "description": "Come and experience the green valleys of Kashimir"
+        "description": "Come and experience the green valleys of Kashimir",
+        "image": "kashmir.jpg"
     },
     {
         "place": "Karachi",
-        "description": "The biggest city in Pakistan, waiting to be discovered"
+        "description": "The biggest city in Pakistan, waiting to be discovered",
+        "image": "karachi.jpg"
     },
     {
         "place": "Fairy Meadow",
-        "description": "The place is said to been inhabited by fairies, one can understand why the choose this place after seeing it for themselves."
+        "description": "The place is said to been inhabited by fairies, one can understand why the choose this place after seeing it for themselves.",
+        "image": "fairyMeadow.jpg"
     },
     {
         "place": "Moenje Daro",
-        "description": "The city of the dead, come and visit one of the old civilations in the world."
+        "description": "The city of the dead, come and visit one of the old civilations in the world.",
+        "image": "moenjadaro.jpg"
     }]
 
 
